43c23e40b2d35a123ae99623a591d35a1e0d2518
Hye-S <gptjs7970@naver.com>
2022-09-01 00:26:10 +0900

    feat: user앱 초기 설정 - 유혜선

---
+++ user/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def test():
+    print("Hello, intial app")
+    return
--- waynehills_D/settings.py
+++ waynehills_D/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
