1f6f2dcf3177ffb94b491bf2ce8a9b9da1a6ac26
Bomi Choi <hibomi97@gmail.com>
2022-09-03 20:47:24 +0900

    Feat: RegisterSerializer validation 추가 - 최보미
    - 입력 필드 조건 추가
    - create 메소드 예외처리 제거

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -7,13 +7,14 @@ from .models import User
 
 
 class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(write_only=True)
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    password_check = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    name = serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True, max_length=159)
+    password = serializers.CharField(write_only=True, max_length=128, style={'input_type': 'password'})
+    password_check = serializers.CharField(write_only=True, max_length=128, style={'input_type': 'password'})
+    name = serializers.CharField(write_only=True, max_length=10)
     email = serializers.EmailField(write_only=True)
-    gender = serializers.ChoiceField(write_only=True, choices=User.GENDER_CHOICES, required=False, allow_null=True)
-    age = serializers.IntegerField(write_only=True, required=False, allow_null=True)
+    gender = serializers.ChoiceField(write_only=True, choices=User.GENDER_CHOICES, required=False,
+                                     allow_null=True)
+    age = serializers.IntegerField(write_only=True, min_value=1, required=False, allow_null=True)
     phone = serializers.CharField(write_only=True, required=False, allow_null=True)
     access = serializers.CharField(read_only=True)
     refresh = serializers.CharField(read_only=True)
@@ -26,26 +27,23 @@ class RegisterSerializer(serializers.Serializer):
         return attrs
 
     def create(self, validated_data):
-        try:
-            # 사용자 생성
-            user = User.objects.create(
-                username=validated_data['username'],
-                password=make_password(validated_data['password']),
-                name=validated_data['name'],
-                email=validated_data['email'],
-            )
+        # 사용자 생성
+        user = User.objects.create(
+            username=validated_data['username'],
+            password=make_password(validated_data['password']),
+            name=validated_data['name'],
+            email=validated_data['email'],
+        )
 
-            # 추가정보 저장
-            user.gender = validated_data.get('gender', None)
-            user.age = validated_data.get('age', None)
-            user.phone = validated_data.get('phone', None)
-            user.save()
+        # 추가정보 저장
+        user.gender = validated_data.get('gender', None)
+        user.age = validated_data.get('age', None)
+        user.phone = validated_data.get('phone', None)
+        user.save()
 
-            # 토큰 발급 후 반환
-            refresh = RefreshToken.for_user(user)
-            return {
-                'access': refresh.access_token,
-                'refresh': refresh
-            }
-        except:
-            raise ValidationError({'create': '오류가 발생했습니다.'})
+        # 토큰 발급 후 반환
+        refresh = RefreshToken.for_user(user)
+        return {
+            'access': refresh.access_token,
+            'refresh': refresh
+        }
